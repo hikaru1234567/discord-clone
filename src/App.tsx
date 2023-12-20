be635d1dc7cf646743e99266dfb7fbe9e2a19e41
hikaru1234567 <makinohikaru0529@gmail.com>
2023-12-20 21:06:51 +0900

    サイドバー完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-    <h1>discord</h1>
+    <Sidebar />
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebarchannel.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Sidebarchannel.scss"
+
+const Sidebarchannel = () => {
+  return (
+    <>
+    <div className='sidebarchannel'>
+      <h4>
+        <span className='sidebarchannelHash'>#</span>
+        Udemy
+      </h4>
+    </div>
+    </>
+  )
+}
+
+
+export default Sidebarchannel
